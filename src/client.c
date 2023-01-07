35886c3ea093698dd4e9d82628599088ff774fcf
ddordain <ddordain@student.42.fr>
2023-01-07 14:45:17 +0100

    comment added

--- src/client.c
+++ src/client.c
@@ -6,36 +6,50 @@
 /*   By: ddordain <ddordain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 14:07:14 by ddordain          #+#    #+#             */
-/*   Updated: 2022/03/04 11:11:35 by ddordain         ###   ########.fr       */
+/*   Updated: 2023/01/07 14:34:57 by ddordain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	termination(int signum)
+/*
+** This function is a signal handler for the SIGUSR2 signal.
+** It does nothing but sleep for a short period of time.
+*/
+static void termination(int signum)
 {
-	(void)signum;
-	usleep(150);
+	(void)signum; // Unused parameter
+	usleep(150);  // Sleep for 150 microseconds
 }
 
-static	void	char_to_binary(pid_t pid, char c)
+/*
+** This function encodes a single character as a series of SIGUSR1 and SIGUSR2 signals,
+** with the value of each signal representing a bit in the character's binary representation.
+*/
+static void char_to_binary(pid_t pid, char c)
 {
-	char	bit;
+	char bit;
 
-	bit = 8;
+	bit = 8; // Each character is 8 bits
 	while (bit--)
 	{
+		// Send a SIGUSR2 signal if the current bit is 1, otherwise send a SIGUSR1 signal
 		if (c >> bit & 1)
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
-		pause();
+		pause(); // Wait for a signal before sending the next one
 	}
 }
 
-static void	encoding_str(pid_t pid, char *str)
+/*
+** This function encodes a string as a series of SIGUSR1 and SIGUSR2 signals
+** by calling the char_to_binary function for each character in the string.
+*/
+
+static void encoding_str(pid_t pid, char *str)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (str[i])
@@ -43,26 +57,31 @@ static void	encoding_str(pid_t pid, char *str)
 		char_to_binary(pid, str[i]);
 		i++;
 	}
-	char_to_binary(pid, '\n');
-	char_to_binary(pid, '\0');
+	char_to_binary(pid, '\n'); // Add a newline character after the string
+	char_to_binary(pid, '\0'); // Add a null terminator after the newline
 }
 
-int	main(int ac, char **av)
+/*
+** The main function is the entry point of the program.
+** It expects two command-line arguments: the PID of the process to which the message should be sent,
+** and the message to be sent.
+*/
+int main(int ac, char **av)
 {
-	pid_t	pid;
+	pid_t pid;
 
-	if (ac != 3)
+	if (ac != 3) // Check if the correct number of arguments were provided
 	{
-		write(2, MSG_ERR_CLT, ft_strlen(MSG_ERR_CLT));
-		return (EXIT_FAILURE);
+		write(2, MSG_ERR_CLT, ft_strlen(MSG_ERR_CLT)); // Print error message to stderr
+		return (EXIT_FAILURE);						   // Return failure code
 	}
-	pid = ft_atoi(av[1]);
-	signal(SIGUSR2, termination);
-	if (kill(pid, 0) == -1)
+	pid = ft_atoi(av[1]);		  // Convert the PID from a string to an integer
+	signal(SIGUSR2, termination); // Set up a signal handler for the SIGUSR2 signal
+	if (kill(pid, 0) == -1)		  // Check if the process with the provided PID exists
 	{
-		write(2, MSG_ERR_PID, ft_strlen(MSG_ERR_PID));
-		return (EXIT_FAILURE);
+		write(2, MSG_ERR_PID, ft_strlen(MSG_ERR_PID)); // Print error message to stderr
+		return (EXIT_FAILURE);						   // Return failure code
 	}
-	encoding_str(pid, av[2]);
-	return (EXIT_SUCCESS);
+	encoding_str(pid, av[2]); // Encode the message and send it to the receiving process
+	return (EXIT_SUCCESS);	  // Return success code
 }
--- src/server.c
+++ src/server.c
@@ -6,54 +6,63 @@
 /*   By: ddordain <ddordain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 13:03:33 by ddordain          #+#    #+#             */
-/*   Updated: 2022/03/04 11:11:37 by ddordain         ###   ########.fr       */
+/*   Updated: 2023/01/07 14:44:41 by ddordain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	handler(int signum, siginfo_t *info, void *unused)
+/*
+** This function is a signal handler for the SIGUSR1 and SIGUSR2 signals.
+** It receives each signal sent by the client and decodes it as a bit in a character.
+** The character is then printed to stdout when all 8 bits have been received.
+*/
+static void handler(int signum, siginfo_t *info, void *unused)
 {
-	static char	c;
-	static int	bit;
+	static char c;	// The character being received
+	static int bit; // The current bit being processed
 
-	(void)unused;
-	bit += 1;
+	(void)unused; // Unused parameter
+	bit += 1;	  // Increment the current bit
 	if (bit == 1)
-		c = '\0';
-	c |= signum == SIGUSR2;
+		c = '\0';			// Reset c to null character if this is the first bit
+	c |= signum == SIGUSR2; // OR c with 1 if the signal is SIGUSR2
 	if (bit < 8)
-		c <<= 1;
-	if (bit == 8)
+		c <<= 1;  // Left-shift c by 1 if there are more bits to receive
+	if (bit == 8) // If all 8 bits have been received
 	{
-		write(1, &c, 1);
-		bit = 0;
+		write(1, &c, 1); // Print c to stdout
+		bit = 0;		 // Reset the current bit
 	}
-	usleep(200);
-	kill(info->si_pid, SIGUSR2);
+	usleep(200);				 // Sleep for 200 microseconds
+	kill(info->si_pid, SIGUSR2); // Send a SIGUSR2 signal to the client to indicate that the server is ready for the next bit
 }
 
-int	main(int ac, char **av)
+/*
+** The main function is the entry point of the program.
+** It expects no command-line arguments.
+*/
+int main(int ac, char **av)
 {
-	pid_t				pid;
-	struct sigaction	action;
+	pid_t pid;				 // The PID of the current process
+	struct sigaction action; // Struct containing information about the signal handler
 
-	(void) av;
-	sigemptyset(&action.sa_mask);
-	action.sa_flags = SA_SIGINFO;
-	action.sa_sigaction = handler;
-	if (ac != 1)
+	(void)av;					   // Unused parameter
+	sigemptyset(&action.sa_mask);  // Initialize the signal set to the empty set
+	action.sa_flags = SA_SIGINFO;  // Set the SA_SIGINFO flag in the sa_flags field
+	action.sa_sigaction = handler; // Set the signal handler function
+	if (ac != 1)				   // Check if any command-line arguments were provided
 	{
-		write(2, MSG_ERR_SRV, ft_strlen(MSG_ERR_SRV));
-		return (EXIT_FAILURE);
+		write(2, MSG_ERR_SRV, ft_strlen(MSG_ERR_SRV)); // Print error message to stderr
+		return (EXIT_FAILURE);						   // Return failure code
 	}
-	pid = getpid();
-	write(1, MSG_PID, ft_strlen(MSG_PID));
-	ft_putnbr_fd(pid, 1);
-	write(1, "\n", 1);
-	sigaction(SIGUSR1, &action, NULL);
-	sigaction(SIGUSR2, &action, NULL);
-	while (42)
-		pause();
+	pid = getpid();						   // Get the PID of the current process
+	write(1, MSG_PID, ft_strlen(MSG_PID)); // Print message to stdout
+	ft_putnbr_fd(pid, 1);				   // Print the PID to stdout
+	write(1, "\n", 1);					   // Print a newline to stdout
+	sigaction(SIGUSR1, &action, NULL);	   // Set up the signal handler for the SIGUSR1 signal
+	sigaction(SIGUSR2, &action, NULL);	   // Set up the signal handler for the SIGUSR2 signal
+	while (42)							   // Infinite loop
+		pause();						   // Wait for a signal
 	return (EXIT_SUCCESS);
 }
